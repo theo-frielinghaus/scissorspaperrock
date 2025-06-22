df16d7a52a03e4a022bf490bb3149f01d6cce271
Theo Frielinghaus <theo.frielinghaus@bsb.hamburg.de>
2025-06-22 18:19:22 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        final var ROUNDS_NO = 100;
+
+        System.out.print("Lets play Scissors, Paper, Rock!");
+
+        GameService gameService = new GameService();
+    }
+}
